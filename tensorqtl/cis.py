Fix for edge cases resulting in loss of precision

tensorqtl/cis.py
@@ -179,7 +179,7 @@ def map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covaria
                         n = len(variant_ids)
 
                         # top association
-                        ix = np.argmax(np.abs(tstat[:,2]))
+                        ix = np.nanargmax(np.abs(tstat[:,2]))
                         top_s = pd.Series([phenotype_id, variant_ids[ix], tss_distance[ix], maf[ix], ma_samples[ix], ma_count[ix],
                                            tstat[ix,0], b[ix,0], b_se[ix,0],
                                            tstat[ix,1], b[ix,1], b_se[ix,1],
@@ -297,7 +297,7 @@ def map_nominal(genotype_df, variant_df, phenotype_df, phenotype_pos_df, covaria
 
                     # top association for the group
                     if interaction_s is not None:
-                        ix = np.argmax(np.abs(tstat[:,2]))
+                        ix = np.nanargmax(np.abs(tstat[:,2]))
                         top_s = pd.Series([chr_res['phenotype_id'][start:start+n][ix], variant_ids[ix], tss_distance[ix], maf[ix], ma_samples[ix], ma_count[ix],
                                            tstat[ix,0], b[ix,0], b_se[ix,0],
                                            tstat[ix,1], b[ix,1], b_se[ix,1],

tensorqtl/core.py
@@ -150,7 +150,7 @@ def calculate_interaction_nominal(genotypes_t, phenotypes_t, interaction_t, resi
     i0_t = residualizer.transform(i0_t, center=False)
     i0_t = i0_t.repeat(ng, 1)
 
-    # regression
+    # regression (in float; loss of precision may occur in edge cases)
     X_t = torch.stack([g0_t, i0_t, gi0_t], 2)  # ng x ns x 3
     Xinv = torch.matmul(torch.transpose(X_t, 1, 2), X_t).inverse() # ng x 3 x 3
     # Xinv = tf.linalg.inv(tf.matmul(X_t, X_t, transpose_a=True))  # ng x 3 x 3