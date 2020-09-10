9d7974b29bc52ff217f4ba10a8f3e119eb36704b
François Aguet <francois-a@users.noreply.github.com>
2020-09-09 21:56:02 -0400

    Updated to PyTorch 1.6 + CUDA 10.2 (Ubuntu 18.04)

--- install/install_cuda.sh
+++ install/install_cuda.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-# Adapted from https://www.tensorflow.org/install/gpu
+# install script for PyTorch 1.6 + CUDA 10.2 combination on Ubuntu 18.04
+# see https://pytorch.org/get-started/locally/
 
-# Add NVIDIA package repositories
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+# see https://developer.nvidia.com/cuda-10.2-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1804&target_type=deblocal
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/local_installers/cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+wget http://developer.download.nvidia.com/compute/cuda/10.2/Prod/patches/1/cuda-repo-ubuntu1804-10-2-local_10.2.1-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-10-2-local_10.2.1-1_amd64.deb
+sudo apt-key add /var/cuda-repo-10-2-local-10.2.89-440.33.01/7fa2af80.pub
 sudo apt-get update
-wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt install -y ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt-get update
+sudo apt-get -y install cuda
 
-# Install NVIDIA driver
-sudo apt-get install -y --no-install-recommends nvidia-driver-410
-# Reboot. Check that GPUs are visible using the command: nvidia-smi
-
-# Install development and runtime libraries (~4GB)
-sudo apt-get install -y --no-install-recommends \
-    cuda-10-0 \
-    libcudnn7=7.4.1.5-1+cuda10.0  \
-    libcudnn7-dev=7.4.1.5-1+cuda10.0
-
-# Install TensorRT. Requires that libcudnn7 is installed above.
-sudo apt-get update && \
-        sudo apt-get install -y nvinfer-runtime-trt-repo-ubuntu1804-5.0.2-ga-cuda10.0 \
-        && sudo apt-get update \
-        && sudo apt-get install -y --no-install-recommends libnvinfer-dev
+python -c "import torch; print('CUDA available: {} ({})'.format(torch.cuda.is_available(), torch.cuda.get_device_name(torch.cuda.current_device())))"
 
 # clean up
-rm cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-rm nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+rm cuda-repo-ubuntu1804-10-2-local-10.2.89-440.33.01_1.0-1_amd64.deb
+rm cuda-repo-ubuntu1804-10-2-local_10.2.1-1_amd64.deb
