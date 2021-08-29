445be8a20166c59177bf1c58c8508e6eccc76910
Francois Aguet <francois@broadinstitute.org>
2021-08-29 05:07:06 +0000

    Significant variant-gene pairs for grouped phenotypes

--- tensorqtl/post.py
+++ tensorqtl/post.py
@@ -204,29 +204,41 @@ def annotate_genes(gene_df, annotation_gtf, lookup_df=None):
     return gene_df
 
 
-def get_significant_pairs(res_df, nominal_prefix, fdr=0.05):
+def get_significant_pairs(res_df, nominal_prefix, group_s=None, fdr=0.05):
     """Significant variant-phenotype pairs based on nominal p-value threshold for each phenotype"""
-    print('['+datetime.now().strftime("%b %d %H:%M:%S")+'] tensorQTL: filtering significant variant-phenotype pairs', flush=True)
+    print('['+datetime.now().strftime("%b %d %H:%M:%S")+'] tensorQTL: parsing all significant variant-phenotype pairs', flush=True)
     assert 'qval' in res_df
 
     # significant phenotypes (apply FDR threshold)
-    df = res_df.loc[res_df['qval']<=fdr, ['pval_nominal_threshold', 'pval_nominal', 'pval_beta']].copy()
+    if group_s is not None:
+        df = res_df.loc[res_df['qval'] <= fdr, ['pval_nominal_threshold', 'pval_nominal', 'pval_beta', 'group_id']].copy()
+        df.set_index('group_id', inplace=True)
+    else:
+        df = res_df.loc[res_df['qval'] <= fdr, ['pval_nominal_threshold', 'pval_nominal', 'pval_beta']].copy()
     df.rename(columns={'pval_nominal': 'min_pval_nominal'}, inplace=True)
     signif_phenotype_ids = set(df.index)
     threshold_dict = df['pval_nominal_threshold'].to_dict()
 
     nominal_files = {os.path.basename(i).split('.')[-2]:i for i in glob.glob(nominal_prefix+'*.parquet')}
-    chroms = sorted(nominal_files.keys(), key=lambda x: int(x.replace('chr','').replace('X','100')))
+    chroms = sorted(nominal_files.keys(), key=lambda x: int(x.replace('chr', '').replace('X', '23')))
     signif_df = []
     for k,c in enumerate(chroms, 1):
-        print(f'  * parsing significant variant-phenotype pairs for chr. {k}/{len(chroms)}', end='\r', flush=True)
+        print(f'  * processing chr. {k}/{len(chroms)}', end='\r', flush=True)
         nominal_df = pd.read_parquet(nominal_files[c])
-        nominal_df = nominal_df[nominal_df['phenotype_id'].isin(signif_phenotype_ids)]
-
-        m = nominal_df['pval_nominal']<nominal_df['phenotype_id'].apply(lambda x: threshold_dict[x])
+        nominal_df = nominal_df[nominal_df['pval_nominal'] <= df['pval_nominal_threshold'].max()]
+        if group_s is not None:
+            nominal_df.insert(1, 'group_id', nominal_df['phenotype_id'].map(group_s))
+            nominal_df = nominal_df[nominal_df['group_id'].isin(signif_phenotype_ids)]
+            m = nominal_df['pval_nominal'] < nominal_df['group_id'].apply(lambda x: threshold_dict[x])
+        else:
+            nominal_df = nominal_df[nominal_df['phenotype_id'].isin(signif_phenotype_ids)]
+            m = nominal_df['pval_nominal'] < nominal_df['phenotype_id'].apply(lambda x: threshold_dict[x])
         signif_df.append(nominal_df[m])
     print()
     signif_df = pd.concat(signif_df, axis=0)
-    signif_df = signif_df.merge(df, left_on='phenotype_id', right_index=True)
+    if group_s is not None:
+        signif_df = signif_df.merge(df, left_on='group_id', right_index=True)
+    else:
+        signif_df = signif_df.merge(df, left_on='phenotype_id', right_index=True)
     print('['+datetime.now().strftime("%b %d %H:%M:%S")+'] done', flush=True)
     return signif_df.reset_index(drop=True)
