e214b2ef3ad3d1928018c33f3b0f37682f46f40d
Francois Aguet <francois@broadinstitute.org>
2022-05-01 00:52:58 +0000

    Added multi-SNP case

--- tensorqtl/mixqtl.py
+++ tensorqtl/mixqtl.py
@@ -8,11 +8,18 @@ from core import *
 
 
 def trc(genotypes_t, counts_t, covariates_t=None, select_covariates=True,
-        count_threshold=0, imputation='offset', return_af=False):
+        count_threshold=0, imputation='offset', mode='standard', return_af=False):
     """
-    genotypes_t: dosages (variants x samples)
-    counts_t: DESeq size factor-normalized read counts
-    covariates_t: first column must be intercept
+    Inputs
+      genotypes_t: dosages (variants x samples)
+      counts_t: DESeq size factor-normalized read counts
+      covariates_t: covariates matrix, first column must be intercept
+      mode: if 'standard', parallel regression for each variant in genotypes_t
+            if 'multi', multiple regression for all variants in genotypes_t
+
+    Outputs:
+      t-statistic, beta, beta_se {af, ma_samples, ma_counts}  (mode='standard')
+      beta, beta_se  (mode='multi')
     """
     nonzero_t = counts_t != 0
 
@@ -44,10 +51,16 @@ def trc(genotypes_t, counts_t, covariates_t=None, select_covariates=True,
         y_t = log_counts_t
 
     m_t = counts_t >= count_threshold
-    res = cis.calculate_cis_nominal(genotypes_t[:, m_t] / 2, y_t[m_t], return_af=False)
 
-    if return_af:
-        af, ma_samples, ma_counts = get_allele_stats(genotypes_t)
-        return *res, af, ma_samples, ma_counts
-    else:
-        return res
+    if mode == 'standard':
+        res = cis.calculate_cis_nominal(genotypes_t[:, m_t] / 2, y_t[m_t], return_af=False)
+        if return_af:
+            af, ma_samples, ma_counts = get_allele_stats(genotypes_t)
+            return *res, af, ma_samples, ma_counts
+        else:
+            return res
+
+    elif mode.startswith('multi'):
+        X_t = torch.cat([torch.ones([m_t.sum(), 1], dtype=bool).to(genotypes_t.device), genotypes_t[:, m_t].T / 2], axis=1)
+        b_t, b_se_t = linreg(X_t, y_t[m_t], dtype=torch.float32)
+        return b_t[1:], b_se_t[1:]
